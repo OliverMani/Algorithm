4647d65375e434e35324e0e49d3d75f61f4289c5
unknown <oliverhilmars@gmail.com>
2020-10-15 12:35:14 +0000

    Lagaði parse fallið, er núna að vinna í heildunarfallinu

--- Verkefni 4/flatarmál.c
+++ Verkefni 4/flatarmál.c
@@ -9,12 +9,13 @@ struct part {
 };
 
 char char_in(char ch, char* str) {
-	int i = 0;
-	while(str[i++])
+	for(int i = 0; str[i]; i++)
 		if(ch == str[i])
 			return 1;
 	return 0;
 }
+
+
 
 char* int_to_str(int num){
 	int size = ceil(log10(num));
@@ -26,6 +27,54 @@ char* int_to_str(int num){
 	}
 
 	return result;
+}
+
+int str_to_int(char* str) {
+	int i = 0;
+	int number = 0;
+	while(str[i]) {
+		number *= 10;
+		number += str[i] - 48;
+		i++;
+	}
+	return number;
+}
+
+double reikna_tolur(char* tolur) {
+	double number = 0;
+	double tmp = 0;
+	int i = 0;
+	while(tolur[i]) {
+		tmp = 0;
+		while(char_in(tolur[i], "0123456789")) {
+			tmp *= 10;
+			tmp += tolur[i] - 48;
+			i++;
+		}
+
+		switch(tolur[i]) {
+			case '+':
+				number += tmp;
+				break;
+			case '-':
+				number -= tmp;
+				break;
+			case '*':
+				number *= tmp;
+				break;
+			case '/':
+				number /= tmp;
+				break;
+			case '^':
+				number = pow(number, tmp);
+				break;
+		}
+
+
+		i++;
+	}
+
+	return number;
 }
 
 struct part* parse(char* formula) {
@@ -46,79 +95,52 @@ struct part* parse(char* formula) {
 
 	while((ch = formula[i++])) {
 
-		printf("ch = %c\n", ch);
-
 		if(char_in(ch, "0123456789")) {
+			
 			if(!read_number) {
 				read_number = 1;
 				last_number = 0;
 			}
-			if(read_variable) {
-				read_variable = 0;
-				printf("Number is NULL: %i (%s -> '%i')\n", last->number == NULL, last->number, last->next_action == 0);
-				struct part* next = malloc(sizeof(struct part));
-				next->number = "0";
-				next->next_action = 0;
-				next->next = NULL;
-				last->next_action = '^';
-				last->next = next;
-				last = last->next;
-				
-				
-			}
-			
-			
-			
-			
-			read_action = 0;
+
 			last_number *= 10;
-			last_number += ch - 48;
+			last_number += (ch - 48);
 
 			last->number = int_to_str(last_number);
-		} else if(char_in(ch, "+-*/^R")) {
-			if(root == last && ch == '-')
-				root->number = "0";
-			//printf("Last next NOT exists: %i\n", last->next == NULL);
-			last->next_action = ch;
+
+		} else if(char_in(ch, "+-/*^R")) {
+			
+			read_number = 0;
+			if(ch == '-' && last == root) {
+				struct part* new = malloc(sizeof(struct part));
+				new->number = "0";
+				new->next_action = ch;
+				new->next = root;
+				root = new;
+			} else {
+				last->next_action = ch;
+				struct part* new = malloc(sizeof(struct part));
+				new->number = NULL;
+				new->next_action = 0;
+				new->next = NULL;
+				last->next = new;
+				last = last->next;
+			}
+		} else {
+			
+			read_number = 0;
+			last->number = "x";
+			if(char_in(formula[i], "+-/*^R")) {
+				last->next_action = formula[i++];
+				
+			} else {
+				last->next_action = char_in(formula[i], "0123456789") ? '^' : '*';
+			}
 			struct part* new = malloc(sizeof(struct part));
 			new->number = NULL;
 			new->next_action = 0;
 			new->next = NULL;
 			last->next = new;
-			
 			last = last->next;
-
-			read_action = 1;
-		} else if(char_in(ch, "()")) {
-
-		} else {
-			// X for example...
-			//printf("?? Formula + 1: %c\n", formula[i+1]);
-
-			char* new_ch = malloc(sizeof(char)*2);
-			new_ch[0] = ch;
-			new_ch[1] = 0;
-
-			if(read_number) {
-				read_number = 0;
-				if(!last->next_action)
-					last->next_action = '*';
-				
-			}
-			
-			last->number = new_ch;
-			if(formula[i + 1] && !char_in(formula[i + 1], "+-/*^R")) {
-				last->next_action = char_in(formula[i+1], "0123456789") ? 'A' : 'B';
-			} else {
-				struct part* new = malloc(sizeof(struct part));
-				new->next_action = 0;
-				last->next = new;
-				last = last->next;
-			}
-
-			read_variable = 1;
-
-
 			
 		}
 	}
@@ -126,20 +148,69 @@ struct part* parse(char* formula) {
 	return root;
 }
 
+void heilda(struct part* root) {
+	struct part* last = root;
+	while(last) {
+		char* number = last->number;
+		char action = last->next_action;
+
+		if(number != "x" && last != root) {
+
+			struct part* new = malloc(sizeof(struct part));
+			new->number = "A";
+			new->next_action = last->next_action;
+			new->next = last->next;
+			last->next_action = '*';
+			last->next = new;
+			
+			last = last->next;
+		} else switch (action) {
+			case '^':
+				// Við viljum setja parta fyrir aftan, byrjum á að býa þá til
+				struct part* tmp = malloc(sizeof(struct part));
+				tmp->number = last->number;
+				tmp->next = last->next;
+				tmp->next_action = last->next_action;
+
+				// Hoppa yfir í næsta
+				last = last->next;
+
+				// Við viljum breyta veldinu...
+				int inum = str_to_int(last->number);
+				inum++;
+				last->number = int_to_str(inum);
+				break;
+			default:break;
+		}
+
+		last = last->next;
+	}
+}
+
 int main() {
 	//printf("Int i streng: %s\n", int_to_str(1234565));
-
+	
 	char input[1024];
 	for(int i = 0; i < 1024; i++)
 		input[i] = 0;
 	gets(input);
 
 	struct part* parsed = parse(input);
+	struct part* last = parsed;
 
-	while(parsed) {
-		printf("%s %c ", parsed->number, parsed->next_action);
-		parsed = parsed->next;
+	while(last) {
+		printf("%s %c ", last->number, last->next_action);
+		last = last->next;
 	}
 
+	printf("\nHeildað: ");
+
+	heilda(parsed);
+	last = parsed;
+
+	while(last) {
+		printf("%s %c ", last->number, last->next_action);
+		last = last->next;
+	}
 	printf("\n");
 }
